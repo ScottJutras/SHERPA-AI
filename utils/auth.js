cdf0b8d626e5438a7eeae7082bb4cbbf5fdd0ca5
ScottJutras <scott@scottjutras.com>
2025-04-08 12:07:35 -0400

    Rename getAuth to getAuthorizedClient

--- utils/auth.js
+++ utils/auth.js
@@ -4,15 +4,24 @@ const credentials = JSON.parse(
   Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64').toString('utf8')
 );
 
-function getAuth() {
+async function getAuthorizedClient() { // Changed from getAuth
   const auth = new google.auth.GoogleAuth({
     credentials,
     scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
   });
-
   return auth.getClient();
 }
 
+async function test() {
+  try {
+    const client = await getAuthorizedClient();
+    console.log("[TEST] Authorized client:", client);
+  } catch (e) {
+    console.error("[TEST] Error:", e.message);
+  }
+}
+test();
+
 module.exports = {
-  getAuth,
+  getAuthorizedClient, // Changed from getAuth
 };
